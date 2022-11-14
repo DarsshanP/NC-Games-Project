dd0000af21c9353e8711cfb35287bef26061ed12
Darsshan <darsshanpalaram@gmail.com>
2022-11-14 15:28:52 +0000

    Adds getReviews endpoint and test

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,17 @@
-const { fetchCategories } = require("../models/model");
+const { fetchCategories, fetchReviews } = require("../models/model");
 
 exports.getCategories = (req, res, next) => {
   fetchCategories()
     .then((categories) => {
       res.send(categories);
+    })
+    .catch(next);
+};
+
+exports.getReviews = (req, res, next) => {
+  fetchReviews()
+    .then((review) => {
+      res.send(review);
     })
     .catch(next);
 };
